85d378c04d798cf07686d2549977641a7da7e224
隔壁老王 <269937035@qq.com>
2017-08-17 14:14:01 +0800

    Redis实现delayQueue

--- src/main/java/com/hz/spring/data/redis/SpringRedisTest.java
+++ src/main/java/com/hz/spring/data/redis/SpringRedisTest.java
@@ -3,15 +3,19 @@ package com.hz.spring.data.redis;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
-import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Collections;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 @RunWith(SpringJUnit4ClassRunner.class)//表示整合JUnit4进行测试
 @ContextConfiguration(locations={"classpath:spring-redis.xml"})//加载spring配置文件
@@ -22,8 +26,11 @@ public class SpringRedisTest {
     @Autowired
     private DefaultRedisScript defaultRedisScript;
 
+    private CountDownLatch countDownLatch=new CountDownLatch(1);
 
-    @Test
+
+
+
     public void springRedisTest(){
 //        redisTemplate.opsForValue().set("aaa","123");
         String key="ess";
@@ -44,6 +51,58 @@ public class SpringRedisTest {
         redisScript.setResultType(Boolean.class);
         System.out.println(redisTemplate.execute(redisScript, Collections.singletonList(key),"11","110","120"));
         System.out.println(redisTemplate.opsForValue().get(key));
+    }
+
+    @Test
+    public void delayQueueRedisTest() throws InterruptedException {
+        startListener();
+        countDownLatch.await();
+    }
+
+
+    private void startListener() {
+        new Thread(() -> {
+            while (true){
+                Long size=redisTemplate.opsForZSet().zCard("delayQueue");
+                List<Object> txResults= (List<Object>) redisTemplate.execute(new SessionCallback() {
+                    @Override
+                    public List<Object> execute(RedisOperations operations) throws DataAccessException {
+                        operations.multi();
+                        long time=System.currentTimeMillis();
+                        operations.opsForZSet().rangeByScore("delayQueue",0,time);
+                        operations.opsForZSet().removeRangeByScore("delayQueue",0,time);
+                        return operations.exec();
+                    }
+                });
+                if(size==null||size==0){
+                    Random random=new Random();
+                    int i = 0;
+                    while (i == 0) {
+                        i = random.nextInt(10);
+                        if (i != 0) {
+                            break;
+                        }
+                    }
+                    System.out.println("i=" + i);
+                    redisTemplate.opsForZSet().add("delayQueue", "a" + i, (System.currentTimeMillis() + ((i) * 1000)));
+                }
+                Set<String> queue= (Set<String>) txResults.get(0);
+                if(!queue.isEmpty()) {
+                    System.out.print("从延迟队列获得的数据:");
+                    for (String str : queue) {
+                        System.out.print(str);
+                    }
+                    System.out.println();
+                }
+                try {
+                    Thread.sleep(300);
+                    continue;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            }
+        }).start();
     }
 
 }
